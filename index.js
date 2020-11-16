5296d020cef066d2936db330bc5a969fe7ac0769
Christopher Bernhard <58096490+cbernhard21@users.noreply.github.com>
2020-11-16 14:03:26 -0600

    worked on the auto-complete widget

--- index.js
+++ index.js
@@ -1,39 +1,27 @@
-import { debounce } from './debounce.js'
-// const getMovieData = async(searchTerm) => {
-//     const response = await axios.get('http://www.omdbapi.com/', {
-//         params: {
-//             apikey: '3618b3e5',
-//             s: searchTerm
-//         }
-//     });
-//        come back and show user movie not found message on screen
-//        if (data.Error) {
-//            return []
-//       }
-//      return response.data.Search;
-// };
-const getMovieBySearch = async(searchTerm) => {
-    const apiKey = '3618b3e5';
-    const search = searchTerm;
-    const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=${search}`);
-    const data = await response.json();
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'd9835cc5',
+            s: searchTerm
+        }
+    });
 
-    //come back and show user movie not found message on screen
-    if (data.Error) {
-        return []
+    if (response.data.Error) {
+        return [];
     }
-    return data.Search;
+
+    return response.data.Search;
 };
 
 const root = document.querySelector('.autocomplete');
 root.innerHTML = `
-    <label><b>Search For A Movie</b></label>
-    <input class="input" />
-    <div class="dropdown">
-        <div class="dropdown-menu">
-            <div class="dropdown-content results"></div>
-        </div>
+  <label><b>Search For A Movie</b></label>
+  <input class="input" />
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
     </div>
+  </div>
 `;
 
 const input = document.querySelector('input');
@@ -41,17 +29,36 @@ const dropdown = document.querySelector('.dropdown');
 const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
-    const movies = await getMovieBySearch(event.target.value);
+    const movies = await fetchData(event.target.value);
 
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
     for (let movie of movies) {
-        const div = document.createElement('div');
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
-        div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}</h1>
-        `;
-        document.querySelector('#target').appendChild(div);
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+      <img src="${imgSrc}" />
+      ${movie.Title}
+  `;
+        option.addEventListener('click', () => {
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+        });
+
+        resultsWrapper.appendChild(option);
     }
 };
-
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+    if (!root.contains(event.target)) {
+        dropdown.classList.remove('is-active');
+    }
+});
